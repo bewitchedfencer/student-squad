bff3e00c80798684ef59036f2670c3a1da6b1610
Mac User <alexarobinson19@gmail.com>
2018-02-17 17:08:31 -0500

    finished messages model and established associations with the Tutor and Teacher tables.

---
+++ models/message.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes){
+    var Message = sequelize.define('message', {
+        author:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[1,140]
+            }
+        },
+        text:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[1, 1500]
+            }
+        },
+        tutor_read:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false,
+            allowNull:false
+        },
+        teacher_read:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false,
+            allowNull:false
+        }
+    });
+    Message.belongsTo(models.Tutor);
+    Message.belongsTo(models.Teacher);
+    return Message;
+    
+}
