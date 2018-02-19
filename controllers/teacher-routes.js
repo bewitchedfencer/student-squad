e5e8b5b2a8bd87facaf49fa49689245244589d83
Mac User <alexarobinson19@gmail.com>
2018-02-19 18:29:21 -0500

    started working on the teacher routes

--- controllers/teacher-routes.js
+++ controllers/teacher-routes.js
@@ -1,17 +1,28 @@
-//TUTOR HOME PAGE
+var express = require("express");
 
-    //PATCH: Add a student - Updates the "tutor-id" column in the student's row to show it corresponds to the tutor that added him/her
-        //1 to many relationship
-        //will add updated student using handlebars partial
+var router = express.Router();
+var db = require("../models/");
 
-    //Google Calendar API - Client Side Only
+    //TEACHER LANDING PAGE
 
-    //GET: View the student profile that was clicked - will send student data to handlebars page
+        //POST: Send message to entire class/classes
+    router.post("/newTeacher", function(req, res){
+        var teacher = req.body;
+        db.Teacher.create({
+            first_name:teacher.firstName,
+            last_name:teacher.lastName,
+            email:teacher.email,
+            password:teacher.password
+        }).then(function(newUser){
+            console.log("New teacher added!");
+        });
+    });
 
-    //GET: Go to Notes page: Get route using handlebars
+        //GET: Get list of classses 
 
+        //Get: list of students in each class - when user clicks on class, generate dropdown with list
 
-//STUDENT PROFILE (Tutor View and Teacher View)
+        //STUDENT PROFILE (Tutor View and Teacher View)
     //GET - Most recent 5 tutor messages/notes from the messages table
 
     //GET - Most recent 5 teacher messages/notes from the messages table
@@ -26,15 +37,7 @@
 
     //GET - Notes / Landing Page (HTML Routes)
 
-//Messages
-    //POST - Create a new tutor message (student name, date, length of session, message, next topics to cover);
+        //Get Pages: Student profile, landing page (HTML Routes)
 
-    //Get Pages: Student profile, landing page (HTML Routes)
-
-    //TEACHER LANDING PAGE
-
-        //POST: Send message to entire class/classes
-
-        //GET: Get list of classses 
-
-        //Get: list of students in each class - when user clicks on class, generate dropdown with list
+    //Messages
+    //POST - Create a new teacher message (student name, date, length of session, message, next topics to cover);
